21f6b48ccca839b10cca6524e857a55bd794b65c
Tevin Mantock <tmantock@gmail.com>
2017-09-09 12:44:38 -0700

    Integer and Boolean types

---
+++ object/object.go
@@ -0,0 +1,18 @@
+package object
+
+import "fmt"
+
+type ObjectType string
+
+type Object interface {
+	Type() ObjectType
+	Inspect() string
+}
+
+type Integer struct {
+	Value int64
+}
+
+func (i *Integer) Inspect() string {
+	return fmt.Sprintf()
+}
